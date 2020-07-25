60df5b3429e734b9a8246259a6653aa4cd2a4eac
Abdulazizsayed <Abdulazizsayed>
2020-07-25 15:35:00 +0200

    Building Folder class

---
+++ models/folder.php
@@ -0,0 +1,73 @@
+<?php
+
+include "database.php";
+
+class Folder
+{
+    private $id;
+    private $title;
+    private $comment_section;
+    private $parent;
+
+    public function set($id = null, $title, $comment_section, $parent = 0)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->comment_section = $comment_section;
+        $this->parent = $parent;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function save()
+    {
+        $con = Database::connect();
+        $query = "INSERT INTO 
+                    folders(title, comment_section, parent)
+                    VALUES(?, ?, ?)";
+        $stmt = $con->prepare($query);
+
+        if ($stmt->execute(array($this->title, $this->comment_section, $this->parent))) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function delete()
+    {
+        $con = Database::connect();
+        $query = "DELETE FROM folders WHERE id = ?";
+        $stmt = $con->prepare($query);
+
+        if ($stmt->execute(array($this->id))) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function update($assocArr)
+    {
+        $updateStr = '';
+
+        foreach ($assocArr as $key => $val) {
+            $updateStr .= $key . " = '" . $val . "', ";
+        }
+
+        $updateStr = substr($updateStr, 0, strlen($updateStr) - 2);
+
+        $con = Database::connect();
+        $query = "UPDATE folders SET $updateStr WHERE id = ?";
+        $stmt = $con->prepare($query);
+
+        if ($stmt->execute(array($this->id))) {
+            return true;
+        }
+
+        return false;
+    }
+}
