a9834f62a90a9b0a7040107ce4b9d9bf013f66c6
Abdulazizsayed <Abdulazizsayed>
2020-07-25 14:24:20 +0200

    Converting Database class from using mysqli to using PDO

--- Database.php
+++ Database.php
@@ -1,36 +1,43 @@
 <?php
-    class Database
-    {        
-        public static function connect($host='localhost', $username='root', $password='14921400', $dbName='bookmark')
-        {
-            $mysqli = new mysqli($host, $username, $password, $dbName); // Check connection
-            if ($mysqli -> connect_errno) 
-            { 
-                echo "Failed to connect to MySQL: " . $mysqli -> connect_error; 
-                exit(); 
-            } 
-        }
-        // public static function save($tableName, $attributes, $values)
-        // {
-        //     $con = self::connect();     
-        //     $tmpAttributes = "";
-        //     for($i = 0 ; $i < sizeof($attributes) ; $i++)
-        //     {
-        //         $tmpAttributes += $attributes[i];
-        //         if($i < sizeof($attributes) - 1)
-        //             $tmpAttributes += ',';
-        //     }
+class Database
+{
+    public static function connect($host = 'localhost', $username = 'root', $password = '', $dbName = 'bookmark')
+    {
+        $options = array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+        );
 
-        //     $tmpValues = "";
-        //     for($i = 0 ; $i < sizeof($tmpValues) ; $i++)
-        //     {
-        //         $tmpValues += $values[i];
-        //         if($i < sizeof($values) - 1)
-        //             $tmpValues += ',';
-        //     }
-            
-        //     $sql = "INSERT INTO $tableName($tmpAttributes) VALUES ($tmpValues)";
-        //     mysqli_query($con, $sql);            
-        // }
-       
+        try {
+            $link = new PDO("mysql:host=$host;dbname=$dbName", $username, $password, $options); // Possible error
+            $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "Failed to connect " . $e->getMessage();
+        }
+
+        return $link;
     }
+
+    // public static function save($tableName, $attributes, $values)
+    // {
+    //     $con = self::connect();     
+    //     $tmpAttributes = "";
+    //     for($i = 0 ; $i < sizeof($attributes) ; $i++)
+    //     {
+    //         $tmpAttributes += $attributes[i];
+    //         if($i < sizeof($attributes) - 1)
+    //             $tmpAttributes += ',';
+    //     }
+
+    //     $tmpValues = "";
+    //     for($i = 0 ; $i < sizeof($tmpValues) ; $i++)
+    //     {
+    //         $tmpValues += $values[i];
+    //         if($i < sizeof($values) - 1)
+    //             $tmpValues += ',';
+    //     }
+
+    //     $sql = "INSERT INTO $tableName($tmpAttributes) VALUES ($tmpValues)";
+    //     mysqli_query($con, $sql);            
+    // }
+
+}
