903073cad69fa614a18be36b0d32dced9a7dac2e
Abdulazizsayed <Abdulazizsayed>
2020-07-26 18:02:25 +0200

    Writing login() function code

---
+++ controllers/loginController.php
@@ -0,0 +1,24 @@
+<?php
+
+class LoginController
+{
+    public function login($username, $password)
+    {
+        $username = filter_var($username, FILTER_SANITIZE_STRING);
+        $password = filter_var($password, FILTER_SANITIZE_STRING);
+
+        $user = new User();
+        $stmt = $user->get("*", "username", $username);
+
+        if ($stmt->rowCount() == 1) {
+            $userData = $stmt->fetch();
+            if (password_verify($password, $userData['password'])) {
+                session_start();
+                $_SESSION['user_data'] = $userData;
+                return true;
+            } else return false;
+        }
+
+        return false;
+    }
+}
